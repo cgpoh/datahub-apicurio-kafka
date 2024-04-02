98ab498729271be74b4335980d6a09da1aa7729d
CG <cgpoh@users.noreply.github.com>
2024-04-02 10:18:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup_output = setup(
     name="apicurio_datahub_kafka",
-    version="1.0.0",
+    version="2.0.0",
     description="DataHub ApiCurio Schema Registry for Kafka Source",
     package_dir={"": "src"},
     packages=find_packages("src"),
