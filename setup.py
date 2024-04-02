a799d0536d16ff54619155cc57c13f5c85d595aa
CG <cgpoh@users.noreply.github.com>
2024-04-02 10:29:55 +0800

    Update setup.py to fix pipeline error

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import find_packages, setup
 
 setup_output = setup(
     name="apicurio_datahub_kafka",
-    version="2.0.0",
+    version="2.0.1",
     description="DataHub ApiCurio Schema Registry for Kafka Source",
     package_dir={"": "src"},
     packages=find_packages("src"),
     install_requires=["acryl-datahub[kafka]"],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
